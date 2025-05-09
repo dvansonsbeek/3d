1b39604269100606c86e8fab9ccee477dce4fbba
dvansonsbeek <98986314+dvansonsbeek@users.noreply.github.com>
2025-05-09 16:14:08 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -4,6 +4,10 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { CSS2DRenderer, CSS2DObject } from 'three/examples/jsm/renderers/CSS2DRenderer.js';
 import * as dat from 'dat.gui';
 
+/*The Interactive 3D Solar System Simulation shows the precession / eccentricity / inclination / obliquity / perihelion date movements of Earth, Moon, Sun and Planets modelled from a geo-heliocentric frame of reference, coming together in a Holistic-Year cycle of 305,952 years, an Axial precession cycle of ~23,535 years, an Inclination precession cycle of 101,984 years and a Perihelion precession cycle of 19,122 years. 
+
+See https://www.holisticuniverse.com/en & https://github.com/dvansonsbeek/3d*/
+
 //*************************************************************
 // ADD GLOBAL INPUT CONSTANTS
 //*************************************************************
