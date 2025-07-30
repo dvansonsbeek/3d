e4ac9bcf9d8880bfe7349563b435774a3a13095d
dvansonsbeek <98986314+dvansonsbeek@users.noreply.github.com>
2025-07-30 14:45:57 +0200

    Update

--- src/lib/startup.ts
+++ src/lib/startup.ts
@@ -1,5 +1,5 @@
 // startup.ts
-import { SimTime } from 'https://github.com/dvansonsbeek/3d/tree/main/src/simTime';
+import { SimTime } from './lib/simTime';
 
 export function initSimTimeFromQuery(url: URL): SimTime {
   const iso = url.searchParams.get('start') || undefined;  // e.g. ?start=2000-06-21T02:00:00Z
